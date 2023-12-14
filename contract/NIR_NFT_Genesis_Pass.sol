7983d5fafb41b0ed68c306b5f8a3c8c1237ca0d8
vitalis-virtus <vshayuk@gmail.com>
2023-12-14 18:34:20 +0200

    made updates in contract

--- contract/NIR_NFT_Genesis_Pass.sol
+++ contract/NIR_NFT_Genesis_Pass.sol
@@ -3,19 +3,15 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NIR_NFT is ERC721, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
-
     uint256 MAX_SUPPLY = 50000;
-    uint256 private _nextTokenId;
-
     string constant TOKEN_URI = "ipfs://bafkreigcjy6bkginsexsjthc2xl4sqsllp453c2bhenx63lsnpw2dvafcm";
 
+    uint256 private _nextTokenId;
+
     constructor(address initialOwner)
-        ERC721("Nirimly Labs (Nirimly Genesis Pass)", "NIR_NFT")
+        ERC721("Nirimly Labs (Nirimly Genesis Pass)", "NIRGP")
         Ownable(initialOwner)
     {}
 
@@ -24,9 +20,8 @@ contract NIR_NFT is ERC721, Ownable {
     }
 
     function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
+        uint256 tokenId = _nextTokenId++;
         require(tokenId <= MAX_SUPPLY, "Sorry, all NFTs have been minted!");
-        _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 }
